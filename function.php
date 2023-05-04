e0d4672ddd6095be40f5eeea1d9e45f1e7730933
Егор Лазукин <130374103+Planet-Coder@users.noreply.github.com>
2023-05-04 21:39:48 +0300

    Update function.php

--- function.php
+++ function.php
@@ -1,7 +1,7 @@
 <?
 class ApiRequest {
     private $header = [
-	'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'Accept-Encoding: gzip, deflate, br',
     'Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
     'Host: www.moex.com',
@@ -13,13 +13,13 @@ class ApiRequest {
     'Sec-Fetch-Site: none',
     'Sec-Fetch-User: ?1',
     'Upgrade-Insecure-Requests: 1',
-	'User-Agent: PostmanRuntime/7.32.2',
-	'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-	'Accept-Encoding: gzip, deflate, br',
-	'Connection: keep-alive',
+    'User-Agent: PostmanRuntime/7.32.2',
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding: gzip, deflate, br',
+    'Connection: keep-alive',
     'Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-	];
-	private $cookie = '_ym_uid=1683127090126929661; _ym_d=1683127090; _ga=GA1.2.397190181.1683127090; _gid=GA1.2.76919165.1683127090; _ym_isad=2';
+    ];
+    private $cookie = '_ym_uid=1683127090126929661; _ym_d=1683127090; _ga=GA1.2.397190181.1683127090; _gid=GA1.2.76919165.1683127090; _ym_isad=2';
     private $apiUrl;
     private $sears = "Объем торгов в ";
     public function __construct($apiUrl) {
@@ -33,13 +33,7 @@ class ApiRequest {
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
-            $response = curl_exec($ch);
-            if($response === false)
-            {
-                echo 'Ошибка curl: ' . curl_error($ch);
-                echo 'Ошибка curl: ' . curl_errno($ch);
-            }
-             
+            $response = curl_exec($ch);             
             curl_close($ch);
             return $response;    
         }
@@ -55,9 +49,9 @@ class ApiRequest {
     }
     public function SearsUrl($response)
     {
-    	include 'simple_html_dom.php';
-		$dom = new simple_html_dom();
-		$dom->load($response);
+        include 'simple_html_dom.php';
+        $dom = new simple_html_dom();
+        $dom->load($response);
 
         $links = $dom->find('a[plaintext^="Объем торгов в"]');
         $AllInfo = "";
@@ -75,12 +69,50 @@ class ApiRequest {
             $dom2->load($Request_Results);
             $elements = $dom2->find('*[plaintext^="Объем торгов на срочном рынке"]');
             $element_plaintext = $elements[0]->plaintext;
+
+            $this->WriteTxtFile($link->plaintext . ',' . $href, $element_plaintext, "savertxt");
+            $pattern = '/\d+,\d+ (млрд|млн|трлн)/';
+            preg_match($pattern, $element_plaintext, $matches);
+            if (!empty($matches)) {
+                $value = $matches[0];
+                $element_plaintext = $value;
+            } else {
+                $element_plaintext = 'Значение не найдено';
+            }
+
+            if (strripos($element_plaintext, 'трлн') !== false) {
+                $element_plaintext = str_replace('трлн', '', $element_plaintext);
+                $element_plaintext = $element_plaintext*1000000000000;
+                // 1000000000000 - трлн
+            }
+            elseif (strripos($element_plaintext, 'млрд') !== false) {
+                $element_plaintext = str_replace('млрд', '', $element_plaintext);
+                $element_plaintext = $element_plaintext*1000000000;
+                // 1000000000 - млрд
+            }
+            elseif (strripos($element_plaintext, 'млн') !== false) {
+                $element_plaintext = str_replace('млн', '', $element_plaintext);
+                $element_plaintext = $element_plaintext*1000000;
+                // 1000000 - млн
+            }
+
             if (empty($AllInfo)) 
-                $AllInfo = $link->plaintext . ';' . $href. ';'. $element_plaintext;
+                $AllInfo = $link->plaintext . ';' . $href . ';' . $element_plaintext;
             else
-                $AllInfo = $AllInfo.PHP_EOL.$link->plaintext . ';' . $href. ';'.$element_plaintext;
+                $AllInfo = $AllInfo.PHP_EOL.$link->plaintext . ';' . $href . ';' . $element_plaintext . " рублей";
         }
         $this->WriteCsvFile($AllInfo, "saver");
+    }
+    public function WriteTxtFile($namefile, $write, $path)
+    {
+        if (is_dir($path)) {
+            $return_new_path = $path.'\\'.str_replace('/', '_', explode('//', explode('?', $namefile)[0])[1]).".txt";
+            file_put_contents($return_new_path, $write, FILE_APPEND);
+            return $return_new_path;
+        }
+        echo "Path not found";
+        return null;
+
     }
     public function WriteCsvFile($write, $path)
     {
